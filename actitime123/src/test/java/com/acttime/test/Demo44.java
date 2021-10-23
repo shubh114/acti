7d7e11978939962a97becd6f68e830a143a2c76d
shubh <shubh@DESKTOP-HUVU21D>
2021-10-23 22:30:27 +0530

    second branch added

---
+++ actitime123/src/test/java/com/acttime/test/Demo44.java
@@ -0,0 +1,19 @@
+package com.acttime.test;
+
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import com.actitime123.generics.BaseClass;
+
+
+public class Demo44 extends BaseClass {
+	@Test
+	public void testDemo() throws InterruptedException {
+		Reporter.log("welcome to testing 1",true);
+		System.out.println("hsis");
+		System.out.println("nkk");
+		System.out.println("cfhhjgj");
+		System.out.println("fmkd");
+	}
+}
